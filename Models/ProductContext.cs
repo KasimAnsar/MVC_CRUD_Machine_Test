3552fd7be416b7ab4e73517cf5dca0061d1537ba
KasimAnsar <117470695+KasimAnsar@users.noreply.github.com>
2023-02-24 15:25:57 +0530

    Add files via upload

---
+++ Models/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.Entity;
+
+namespace MVC_CRUD_Machine_Test.Models
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Products> products { get; set; }
+    }
+}
